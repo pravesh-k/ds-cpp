b86721a6f4fdf5c32713bbfa6f46ce0cbe3e9742
Pravesh <praveshnayak@gmail.com>
2021-10-08 22:18:13 +0530

    added: comments

--- code_linked_list/nth_num_with_prime_digits.cpp
+++ code_linked_list/nth_num_with_prime_digits.cpp
@@ -1,8 +1,12 @@
+// Program to find nth number with all digits as prime numbers
+
 #include<iostream>
 using namespace std;
 
 class Solution{
     public:
+
+    // function to find the nth number with all digits prime
     int theNumber(int n){
         int num = 1, i = 0;
         while(i != n){
@@ -16,18 +20,19 @@ class Solution{
         return num;
     }
     
+    // boolean function to determine whether a number has all digits prime or not
     bool allDigitPrime(int num){
         
         bool flag = false;
         while(num){
             int dig = num % 10;
             
-            if((dig == 2) || (dig == 3) || (dig == 5) || (dig == 7)){
+            if((dig == 2) || (dig == 3) || (dig == 5) || (dig == 7)){       // single digits prime only can be 2,3,5,7
                 flag = true;
                 num = num/10;
             }
             else{
-                flag = false;
+                flag = false;       // if any digit is not prime, break the loop and return false
                 break;
             }
         }
@@ -35,6 +40,7 @@ class Solution{
     }
 };
 
+// driver code
 int main()
  {
 	//code
